5211084eed88b973e9c5c95c55299c9f060ab0f5
Javan Saldanha <javan.saldanha@softplan.com.br>
2024-12-04 23:27:30 -0300

    remover projeto do estado

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,8 +35,7 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(prevRepos => prevRepos.filter(repo => repo.id !== id));
   }
 
 
